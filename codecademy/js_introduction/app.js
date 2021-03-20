d96c5a30dafaf293c3ccb4375309fae6891712fb
Rafael Langthaler <langthaler.r@gmx.at>
2021-03-20 21:09:30 +0100

    string concatenation

--- codecademy/js_introduction/app.js
+++ codecademy/js_introduction/app.js
@@ -1,4 +1,2 @@
-console.log(28+3.5);
-console.log(2021-1969); //ys since moon landing
-console.log(65/240);
-console.log(0.2708*100);
+console.log('Hello' + 'World');
+console.log('Hello '+ 'World');
