0e6e189019f83af9e8eeae39905ed31f1d6188cc
Rafael Langthaler <langthaler.r@gmx.at>
2021-03-25 16:13:27 +0100

    tileLayer

--- nz/main.js
+++ nz/main.js
@@ -1,5 +1,12 @@
-console.log("Hello world!");
-console.log(L);
 
-const map = L.map("map");
+//-43.883333, 170.516667  south east
+
+const map = L.map("map", {
+    center: [ -43.883333, 170.516667 ],
+    zoom: 13,
+    layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    ]
+    // {s} bedeutet server, um traffic auf server von openstreetmap abzuregeln
+});
 console.log(document.querySelector("#map"));
