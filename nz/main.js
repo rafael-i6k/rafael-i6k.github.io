54aaa8d0181c060014ab20bbff4f5f3988669cc1
Rafael Langthaler <langthaler.r@gmx.at>
2021-03-25 16:20:21 +0100

    marker hinzugefügt

--- nz/main.js
+++ nz/main.js
@@ -8,6 +8,9 @@ const map = L.map("map", {
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 });
+
+let mrk = L.marker([-43.883333, 170.516667]).addTo(map);
+
 console.log(document.querySelector("#map"));
 
 // {s} bedeutet server, um traffic auf server von openstreetmap abzuregeln
