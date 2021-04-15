785311f333e099eaba6f175b5b18b083810ec4a4
Rafael Langthaler <langthaler.r@gmx.at>
2021-04-15 17:08:01 +0200

    neue Overlay Layer

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -24,21 +24,29 @@ let layerControl = L.control.layers({
 
 let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
 
+let awsLayer = L.featureGroup();
+layerControl.addOverlay(awsLayer, "Wetterstationen Tirol");
+
+
 fetch(awsUrl)
     .then(response => response.json())
     .then(json => {
         console.log('Daten konvertiert: ', json);
         for (station of json.features) {
             console.log('Station: ', station);
-            let marker = L.marker([station.geometry.coordinates[1], 
+            let marker = L.marker([
+                station.geometry.coordinates[1], 
                 station.geometry.coordinates[0]]
                 );
-            marker.addTo(map);
+                //man muss bei marker die Reihenfolge der Koordinaten ändern, bei marker zuerst länge und dann breite...glaub ich
+                marker.bindPopup(`<h3>${station.properties.name}</h3>`);
+                marker.addTo(map);
         }
 });
 
-//fetch holt daten von URL, nach dem Daten geholt wurden führt then eine nächste funktion aus
 
+//fetch holt daten von URL, nach dem Daten geholt wurden führt then eine nächste funktion aus
+//man muss abwarten was von der Leitung kommt, dies ist die Response, 
 //https://lawine.tirol.gv.at/data/produkte/ogd.geojson falscher link
 
 //https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson
