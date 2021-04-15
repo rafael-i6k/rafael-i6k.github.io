168486005f17c0794dcb2b1ba23c7ed12d97ff1c
Rafael Langthaler <langthaler.r@gmx.at>
2021-04-15 16:47:06 +0200

    marker eingefügt

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -16,4 +16,29 @@ let layerControl = L.control.layers({
     'BasemapAT.highdpi': L.tileLayer.provider('BasemapAT.highdpi'),
     'BasemapAT.basemap': L.tileLayer.provider('BasemapAT.basemap'),
     'BasemapAT.surface': L.tileLayer.provider('BasemapAT.surface'),
+    'BasemapAT.overlay+ortho': L.layerGroup([
+        L.tileLayer.provider('BasemapAT.orthofoto'),
+        L.tileLayer.provider('BasemapAT.overlay'),
+    ])
 }) .addTo(map);
+
+let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
+
+fetch(awsUrl)
+    .then(response => response.json())
+    .then(json => {
+        console.log('Daten konvertiert: ', json);
+        for (station of json.features) {
+            console.log('Station: ', station);
+            let marker = L.marker([station.geometry.coordinates[1], 
+                station.geometry.coordinates[0]]
+                );
+            marker.addTo(map);
+        }
+});
+
+//fetch holt daten von URL, nach dem Daten geholt wurden führt then eine nächste funktion aus
+
+//https://lawine.tirol.gv.at/data/produkte/ogd.geojson falscher link
+
+//https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson
