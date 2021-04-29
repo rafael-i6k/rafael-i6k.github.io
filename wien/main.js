977a7a0ac5568b7f2d516a5985fbfb35ac64889b
rafael-i6k <79929013+rafael-i6k@users.noreply.github.com>
2021-04-29 16:44:42 +0200

    Add files via upload

---
+++ wien/main.js
@@ -0,0 +1,96 @@
+// OGD-Wien Beispiel
+
+// Kartenhintergründe der basemap.at definieren
+let baselayers = {
+    standard: L.tileLayer.provider("BasemapAT.basemap"),
+    grau: L.tileLayer.provider("BasemapAT.grau"),
+    terrain: L.tileLayer.provider("BasemapAT.terrain"),
+    surface: L.tileLayer.provider("BasemapAT.surface"),
+    highdpi: L.tileLayer.provider("BasemapAT.highdpi"),
+    ortho_overlay: L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ]),
+};
+
+// Overlays für die Themen zum Ein- und Ausschalten definieren
+let overlays = {
+    busLines: L.featureGroup(),
+    busStops: L.featureGroup(),
+    pedAreas: L.featureGroup()
+};
+
+// Karte initialisieren und auf Wiens Wikipedia Koordinate blicken
+let map = L.map("map", {
+    center: [48.208333, 16.373056],
+    zoom: 13,
+    layers: [
+        baselayers.grau
+    ]
+});
+
+// Kartenhintergründe und Overlays zur Layer-Control hinzufügen
+let layerControl = L.control.layers({
+    "basemap.at Standard": baselayers.standard,
+    "basemap.at grau": baselayers.grau,
+    "basemap.at Relief": baselayers.terrain,
+    "basemap.at Oberfläche": baselayers.surface,
+    "basemap.at hochauflösend": baselayers.highdpi,
+    "basemap.at Orthofoto beschriftet": baselayers.ortho_overlay
+}, {
+    "Liniennetz Vienna Sightseeing": overlays.busLines,
+    "Haltestellen Vienna Sightseeing": overlays.busStops,
+    "Fußgängerzonen": overlays.pedAreas
+}).addTo(map);
+
+// alle Overlays nach dem Laden anzeigen
+overlays.busLines.addTo(map);
+overlays.busStops.addTo(map);
+overlays.pedAreas.addTo(map);
+
+let drawBusStop = (geojsonData) => {
+    L.geoJson(geojsonData, {
+        onEachFeature: (feature, layer) => { //https://leafletjs.com/reference-1.7.1.html#geojson 
+            layer.bindPopup(feature.properties.STAT_NAME)
+        },
+        pointToLayer: (geoJsonPoint, latlng) => {
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: 'icons/busStop.png',
+                    iconSize: [38, 38]
+                })
+            })
+        }
+    }).addTo(overlays.busStops);
+}
+
+/*
+fetch("data/TOURISTIKHTSVSLOGD.json")
+    .then(response => response.json())
+    .then(stations => {
+        L.geoJson(stations, {
+            onEachFeature: (feature, layer) => {   //https://leafletjs.com/reference-1.7.1.html#geojson 
+                layer.bindPopup(feature.properties.STAT_NAME)
+            },
+            pointToLayer: (geoJsonPoint, latlng) => {
+                return L.marker(latlng, {
+                    icon: L.icon({
+                        iconUrl: 'icons/busStop.png',
+                        iconSize: [38, 38]
+                    })
+                })
+            }
+        }).addTo(map);
+    })  */
+
+for (let config of OGDWIEN) { //Schleife über OGDWien, um auszugeben was da drin steht
+    console.log("Config: ", config.data);
+    fetch(config.data)
+        .then(response => response.json()) //konvertieren von den Daten, die kommen, in json Format, damit function startet --> ()
+        .then(geojsonData => {
+            console.log("Data: ", geojsonData);
+            if (config.title == "Haltestellen Vienna Sightseeing") {
+                drawBusStop(geojsonData);
+            }
+        })
+}
