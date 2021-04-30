3727b98fda1dc7dd5c1e5f7ea7999a06d87a8b75
Rafael Langthaler <langthaler.r@gmx.at>
2021-04-30 10:28:13 +0200

    the .length property

--- codecademy/js_arrays/main.js
+++ codecademy/js_arrays/main.js
@@ -1,9 +1,2 @@
-let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
-condiments[0] = 'Mayo';
-console.log(condiments);
-condiments = ['Mayo'];
-console.log(condiments);
-
-const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
-utensils[3] = 'Spoon';
-console.log(utensils);
+const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
+console.log(objectives.length);
