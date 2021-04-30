d3aa1635a47028333cd1367407c9d7a379b35b5b
Rafael Langthaler <langthaler.r@gmx.at>
2021-04-30 10:54:20 +0200

    more array methods

--- codecademy/js_arrays/main.js
+++ codecademy/js_arrays/main.js
@@ -1,4 +1,14 @@
-const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+//shift() deletes first item of an array
+groceryList.shift();
+console.log(groceryList);
+//unshift() adds an item at the beginning of array
+groceryList.unshift('popcorn');
+console.log(groceryList);
+//slice() copies array with index range in brackets
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);
 
-chores.pop();
-console.log(chores);
+const pastaIndex = groceryList.indexOf('pasta');
+
+console.log(pastaIndex);
