6f6762f4cb86378bd58f4be64bda1736fd8f434f
Rafael Langthaler <langthaler.r@gmx.at>
2021-04-30 11:05:48 +0200

    nested arrays

--- codecademy/js_arrays/main.js
+++ codecademy/js_arrays/main.js
@@ -1,13 +1,2 @@
-const concept = ['arrays', 'can', 'be', 'mutated'];
-
-function changeArr(arr){
-  arr[3] = 'MUTATED';
-}
-
-changeArr(concept);
-console.log(concept);
-function removeElement(newArr) {
-  newArr.pop();
-}
-removeElement(concept);
-console.log(concept);
+var numberClusters = [[1, 2], [3, 4], [5, 6]];
+const target = numberClusters[2][1];
