33bb2b3f4e9d1102c9b08f93bdce8a15605f7f2f
Rafael Langthaler <langthaler.r@gmx.at>
2021-06-02 12:22:09 +0200

    data organised

---
+++ projektversuch/badegewaesser.js
@@ -0,0 +1,6 @@
+
+const BADEGEWAESSER = [{
+    title: "Badegewaesser Oesterreich",
+    source: "https://www.data.gv.at/katalog/dataset/badegewaesser/resource/7a90ef57-8cb2-4d62-a474-aa2085733de9",
+    data: "data/badegewaesser_db.json"
+}]
